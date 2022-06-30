ff4e32cead0666ff159fb14ca8a748a929a84fd8
quantumbeep <kh@khs-mac-mini.lan>
2022-06-29 17:05:10 -0700

    add event listener to grid box with color change

--- app.js
+++ app.js
@@ -14,17 +14,20 @@ const playerFactory = (firstName, lastName, birthDate) => {
     }
     return age;
   };
-  const turnCounter = () => {
-    let turns = 0;
-    turns++;
-    return turns;
+  const setTurn = () => {
+    return (isActiveTurn = !isActiveTurn);
   };
-  const placeMarker = () => {
-    //select grid item dom
-    //if item is empty then change the color of the item background
+  const placeMarker = (box) => {
+    //select grid box dom
+
+    const x = document.querySelectorAll('.box');
+    x.onclick = (e) => {
+      console.log(e.target);
+
+    }
+    //if box is empty then change the color of the box background
     //add count to turns
     //else display message or indicator that space is taken already
-    //
   };
   return {
     firstName,
@@ -32,12 +35,44 @@ const playerFactory = (firstName, lastName, birthDate) => {
     birthDate,
     wins,
     totalGamesPlayed,
-    getFullName,
+    isActiveTurn,
+    getPlayerData,
     getAge,
   };
 };
 
 //gameboard factory
 const gameBoardFactory = (size) => {
-  const createBoard = () => {};
+  const calcSize = () => {
+    gridSize = size ** 2;
+    return gridSize;
+  };
+
+  const createBox = (i) => {
+    const gridContainer = document.querySelector('.grid');
+    const box = document.createElement('div');
+    box.classList.add('box-' + i, 'box');
+    box.setAttribute('box-id', i);
+    box.textContent = 'box-' + i;
+    gridContainer.append(box);
+    box.onclick = () => {
+      const boxToMark = document.querySelector(`[box-id="${i}"]`);
+      boxToMark.style.background = "red";
+    };
+  };
+
+  const createBoard = () => {
+    const boxCount = calcSize();
+    for (let i = 1; i <= boxCount; i++) {
+      createBox(i);
+    }
+  };
+  return {
+    createBoard,
+  };
 };
+
+const game = gameBoardFactory(3);
+
+
+game.createBoard();
