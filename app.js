4f765c5619eb463f6b4719f9e2410d11416ae8f0
quantumbeep <kh@khs-mac-mini.lan>
2022-07-01 11:02:27 -0700

    add dynamic functioning check of winning row

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const playerFactory = (firstName, lastName, birthDate) => {
     }
     return age;
   };
+
   const setTurn = () => {
     return (isActiveTurn = !isActiveTurn);
   };
@@ -23,8 +24,7 @@ const playerFactory = (firstName, lastName, birthDate) => {
     const x = document.querySelectorAll('.box');
     x.onclick = (e) => {
       console.log(e.target);
-
-    }
+    };
     //if box is empty then change the color of the box background
     //add count to turns
     //else display message or indicator that space is taken already
@@ -44,8 +44,8 @@ const playerFactory = (firstName, lastName, birthDate) => {
 //gameboard factory
 const gameBoardFactory = (size) => {
   const calcSize = () => {
-    gridSize = size ** 2;
-    return gridSize;
+    boxCount = size ** 2;
+    return boxCount;
   };
 
   const createBox = (i) => {
@@ -53,26 +53,74 @@ const gameBoardFactory = (size) => {
     const box = document.createElement('div');
     box.classList.add('box-' + i, 'box');
     box.setAttribute('box-id', i);
+    box.setAttribute('data-marked', '');
     box.textContent = 'box-' + i;
     gridContainer.append(box);
-    box.onclick = () => {
-      const boxToMark = document.querySelector(`[box-id="${i}"]`);
-      boxToMark.style.background = "red";
+    console.log(gridContainer);
+    box.onclick = (e) => {
+      const boxToMark = e.target;
+      // const boxToMark = document.querySelector(`[box-id="${i}"]`);
+      boxToMark.style.background = 'red';
+      box.setAttribute('data-marked', true);
+      console.log(box);
+      console.log(boxToMark);
+      console.log(gridContainer);
+      const isMarked = box.dataset.marked !== '';
+      checkWin();
     };
+  };
+ const resetGame = () => {
+
+ }
+ 
+  const checkWin = () => {
+    //grab grid
+    const gridContainer = document.querySelector('.grid');
+
+    //access all boxes via childnodes and assign to const[]
+    const boxArray = gridContainer.childNodes;
+    const newArr = Array.from(boxArray);
+    console.log(typeof newArr);
+    console.log(newArr);
+    //map over array to extract 'marked' attribute into new array
+    const markedArray = newArr.map((box) => box.dataset.marked);
+    //does the row have empty string in marked attribute
+
+    let size = 3;
+
+    let from = 0;
+    let to = 3;
+    let winningLine = false;
+    for (let i = 0; i <= size - 1; i++) {
+      winningLine = !markedArray.slice(from, to).includes('');
+      if (winningLine) {
+        console.log('winner!');
+        resetGame()
+        return;
+      }
+      console.log(winningLine);
+      from += size;
+      to += size;
+      console.log(from, to);
+    }
+    console.log(markedArray);
+    console.log(winningLine);
   };
 
   const createBoard = () => {
-    const boxCount = calcSize();
-    for (let i = 1; i <= boxCount; i++) {
+    for (let i = 1; i <= calcSize(); i++) {
       createBox(i);
     }
   };
+
   return {
     createBoard,
+    checkWin,
   };
 };
 
 const game = gameBoardFactory(3);
-
+console.trace(game);
 
 game.createBoard();
+game.checkWin();
